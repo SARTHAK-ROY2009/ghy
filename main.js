eb4818e29fe5a21eb08e6e33b1b41b23f480b09c
SARTHAK-ROY2009 <97378801+SARTHAK-ROY2009@users.noreply.github.com>
2022-10-29 12:10:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var song = "";
+var music = "";
+
+function preload() {
+    song = loadSound("song.mp3");
+    music = loadSound("music.mp3");
+}
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
